d7844d1fa8a4d6215b743365d2a662b3907d0f42
chrisss101 <Crafaniello9535@bths.edu>
2018-10-01 10:12:50 -0400

    htrsthsr

---
+++ src/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle extends main{
+    private int side;
+    public Triangle() {
+
+    }
+    public double getPerimeter() {
+        return side*3;
+    }
+    public double getArea() {
+        return ((side*side) / 2);
+    }
+}
