aaf7639057ce9d3f137cad6416509922beda2e9b
BT_1N3_31 <cyang4980@bths.edu>
2018-10-02 09:31:25 -0400

    Rect and Shape

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,22 @@
-public class Rectangle {
+public class Rectangle extends Shape{
+
+    private int width;
+    private int length;
+
+    public Rectangle(int width, int length)
+    {
+        super("");
+
+    }
+    @Override
+    public double getArea()
+    {
+        return (width * length);
+    }
+
+    @Override
+    public double getPerimeter()
+    {
+        return (width + length)*2;
+    }
 }
